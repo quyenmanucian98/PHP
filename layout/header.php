2b059d42546ced31375b54cc54f453cb750bbe70
quyenmanucian98 <ledanhquyen1998@gmail.com>
2019-10-02 09:59:17 +0700

    LayoutWep

---
+++ layout/header.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tạo Layout cho Wep</title>
+    <style>
+        div.container {
+            width: 300px;
+            border: 1px solid red;
+        }
+
+        header, footer {
+            padding: 1em;
+            color: aqua;
+            background-color: black;
+            clear: left;
+            text-align: center;
+        }
+        nav ul{
+            list-style-type: none;
+            padding: 0;
+        }
+        nav ul a{
+            text-decoration: none;
+        }
+        article{
+            margin-left: 170px;
+            border-left: 1px solid red;
+            padding: 1em;
+            overflow: hidden;
+        }
+    </style>
+</head>
+<body>
+</body>
+</html>
---
+++ layout/nav.php
@@ -0,0 +1,6 @@
+<ul>
+    <h1>City Gallery</h1>
+    <li><a href="#"></a>London</li>
+    <li><a href="#"></a>Paris</li>
+    <li><a href="#"></a>Tokyo</li>
+</ul>
